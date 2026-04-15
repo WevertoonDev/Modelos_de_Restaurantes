f55827037bf5246b4f9577c8e562b3761efbfcfb
Weverton Martins <wevertonm22@icloud.com>
2026-04-14 22:04:44 -0300

    corrigindo o self

--- modelos/Cardapio/bebida.py
+++ modelos/Cardapio/bebida.py
@@ -3,5 +3,6 @@ from modelos.cardapio.item_cardapio import itemCardapio
 class Bebida(itemCardapio):
     def __init__(self,nome,preco,tamanho):
         super().__init__(nome,preco)
-        self.descricao = descricao
+        self.tamanho = tamanho
+        
     
